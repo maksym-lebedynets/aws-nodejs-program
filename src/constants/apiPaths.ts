9a0009ea219e1461d2f6ba184329f78b704450ad
Maksym Lebedynets <maksym_lebedynets@epam.com>
2020-12-09 19:19:39 +0200

    Added integration with Cart service (AWS Beanstalk)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,8 @@ const API_PATHS = {
   product: 'https://yp0ekii2cc.execute-api.eu-west-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://12zclhaphd.execute-api.eu-west-1.amazonaws.com/dev/import',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://maksym-lebedynets-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
